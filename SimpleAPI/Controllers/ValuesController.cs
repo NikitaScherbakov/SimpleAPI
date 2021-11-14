0778032ba13eec49818214073d0efe7f3e556acb
Nikita <layzendango@gmail.com>
2021-11-14 19:24:45 +0300

    Trigger pipeline

--- SimpleAPI/Controllers/ValuesController.cs
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -6,6 +6,12 @@ namespace SimpleAPI.Controllers
     [Route("[controller]")]
     public class ValuesController
     {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Nikita";
+        }
+        
         [HttpGet("{id:int}")]
         public ActionResult<string> Get(int id)
         {
